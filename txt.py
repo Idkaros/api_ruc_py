3f62b10a1b58df86ab3768f14f84d5369f448f67
Virgilio Céspedes <virgilio.cespedes@bancontinental.com.py>
2023-10-23 16:23:24 -0300

    método que ya no uso

--- txt.py
+++ txt.py
@@ -9,27 +9,27 @@ carpeta_actual = os.getcwd()
 # Concatenar la ruta actual con la carpeta "txt_files"
 carpeta_txt = os.path.join(carpeta_actual, "txt_files")
 
-def insertar():
-
-    # Listar todos los archivos en la carpeta actual
-    files = os.listdir(carpeta_txt)
-
-    # Recorrer los archivos
-    for file in files:
-        # Validar que el archivo sea txt
-        if file.endswith(".txt"):
-            # Ruta de 1 archivo txt
-            txt_file_path = os.path.join(carpeta_txt, file)
-
-            # Abrir el archivo
-            with open(txt_file_path, "r", encoding="utf-8") as archivo:
-                # Recorrer las líneas
-                for linea in archivo:
-                    ruc, apellinombre, dv, codigo, estado, fin = linea.strip().split(
-                        "|"
-                    )
-                    sq.insertar_txt(ruc, apellinombre, dv, codigo, estado)
-            os.remove(txt_file_path)
+#def insertar():
+#
+#    # Listar todos los archivos en la carpeta actual
+#    files = os.listdir(carpeta_txt)
+#
+#    # Recorrer los archivos
+#    for file in files:
+#        # Validar que el archivo sea txt
+#        if file.endswith(".txt"):
+#            # Ruta de 1 archivo txt
+#            txt_file_path = os.path.join(carpeta_txt, file)
+#
+#            # Abrir el archivo
+#            with open(txt_file_path, "r", encoding="utf-8") as archivo:
+#                # Recorrer las líneas
+#                for linea in archivo:
+#                    ruc, apellinombre, dv, codigo, estado, fin = linea.strip().split(
+#                        "|"
+#                    )
+#                    sq.insertar_txt(ruc, apellinombre, dv, codigo, estado)
+#            os.remove(txt_file_path)
 
 def insertar1raVez():
     # Insertamos todo lo que está en los txt's, asumiendo que la BD está vacia
